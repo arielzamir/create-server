99e45234e7a258c8b0af72d41c1a540b58dc4458
Ariel Zamir <arielzamir100@gmail.com>
2023-08-16 10:28:40 +0300

    Added after finishing stage2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const data = require("./data");
+const { v4: uuid4 } = require("uuid");
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -9,7 +10,7 @@ app.get("/users", (req, res) => {
   res.json(data);
 });
 
-// Read user by ID
+//Read user by ID
 app.get("/users/:id", (req, res) => {
   const userId = req.params.id;
   data.forEach((user) => {
@@ -19,25 +20,32 @@ app.get("/users/:id", (req, res) => {
   });
 });
 
-// Create a new user
+//Create a new user
 app.post("/new-user", (req, res) => {
-  const newUser = req.body;
+  const newUser = {
+    id: uuid4(),
+    email: req.body.email,
+    password: req.body.password,
+  };
   data.push(newUser);
   res.send(data);
 });
 
-// Update user by ID
+// // Update user by ID
 app.put("/update-user/:id", (req, res) => {
   const userId = req.params.id;
   const updatedUser = req.body;
   const userIndex = data.findIndex((user) => user.id === userId);
-  data[userIndex].id = updatedUser.id;
+  if (userIndex === -1) {
+    return res.status(404).json({ message: "User not found" });
+  }
   data[userIndex].email = updatedUser.email;
   data[userIndex].password = updatedUser.password;
+
   res.send(data);
 });
 
-// Delete user by ID
+// // Delete user by ID
 app.delete("/delete-user/:id", (req, res) => {
   const userId = req.params.id;
   const newArray = [];
--- data.js
+++ data.js
@@ -1,16 +1,18 @@
+const { v4: uuid4 } = require("uuid");
+
 const array = [
   {
-    id: "1",
+    id: uuid4(),
     email: "someone1@gmail.com",
     password: "1234",
   },
   {
-    id: "2",
+    id: uuid4(),
     email: "someone2@gmail.com",
     password: "123456",
   },
   {
-    id: "3",
+    id: uuid4(),
     email: "someone3@gmail.com",
     password: "12345678",
   },
